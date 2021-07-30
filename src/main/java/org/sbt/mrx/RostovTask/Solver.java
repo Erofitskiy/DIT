888e67bc6ac5621b4c14efbca72b432daedc6988
MRXover <14204072+MRXover@users.noreply.github.com>
2021-07-30 14:19:45 +0300

    imaginary roots

--- src/main/java/org/sbt/mrx/RostovTask/Solver.java
+++ src/main/java/org/sbt/mrx/RostovTask/Solver.java
@@ -7,6 +7,20 @@ import java.util.Scanner;
 public class Solver {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("a*x^2 + b*x + c = 0");
+        System.out.print("a = ");
+        double a = scanner.nextDouble();
+        System.out.print("b = ");
+        double b = scanner.nextDouble();
+        System.out.print("c = ");
+        double c = scanner.nextDouble();
+        try {
+            List<Double> solutions = solveQuadraticEquation(a, b, c);
+            for (double x : solutions)
+                System.out.println("x = " + x);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
     public static List<Double> solveQuadraticEquation(double a, double b, double c) throws Exception {
@@ -15,8 +29,15 @@ public class Solver {
             result.add(solveLinearEquation(b, c));
         else{
             double D = b*b - 4*a*c;
-            if ( D < 0){
+            if ( D < 0 ){
                 System.out.println("No real roots");
+                if ( a > 0 ){
+                    System.out.println("x1 = " + (-b/2/a) + " + " + Math.sqrt(-D)/2/a + "i");
+                    System.out.println("x2 = " + (-b/2/a) + " - " + Math.sqrt(-D)/2/a + "i");
+                } else {
+                    System.out.println("x1 = " + (-b/2/a) + " - " + Math.sqrt(-D)/2/Math.abs(a) + "i");
+                    System.out.println("x2 = " + (-b/2/a) + " + " + Math.sqrt(-D)/2/a + "i");
+                }
             } else {
                 result.add( (-b + Math.sqrt(D)) / (2*a) );
                 result.add( (-b - Math.sqrt(D)) / (2*a) );
